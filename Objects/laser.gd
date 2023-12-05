6b54618aaa1eca1aa2022ebddbeca6651a429d1c
TeddyManGuy <77462538+kadenmork@users.noreply.github.com>
2023-12-05 13:19:23 -0600

    Connected laser to level, made laser disappear after 2s

--- Objects/laser.gd
+++ Objects/laser.gd
@@ -3,5 +3,20 @@ extends Area2D
 @export var speed: int = 1200
 var direction: Vector2 = Vector2.UP
 
+
+func _ready():
+	$laserLife.start()
+
 func _process(delta):
 	position += direction * speed * delta
+
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	queue_free()
+
+
+
+func _on_laser_life_timeout():
+	queue_free()
